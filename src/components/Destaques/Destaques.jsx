d583a6fa42968f9d1462577acbeb1d984c3d810e
SousaPatricia25 <patricia.sousa2413@outlook.com>
2025-06-13 16:31:03 -0300

    Destaques e card

---
+++ src/components/Destaques/Destaques.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './Destaques.css'
+
+export const Destaques = ({ children }) => (
+  <div className="destaques">
+    <h2>Conteúdos em Destaque</h2>
+    <div className="conteúdos">
+      {children}
+    </div>
+  </div>
+);
+
+export const Destaques = ({discount, button, })
+
+
+return (
+
