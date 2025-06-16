8a5e4673a367206914dff118754220e3a81b0f72
PatriciaCosta9852 <arquivosparanuvem3112@gmail.com>
2025-06-16 14:33:34 -0300

    Inicio dos Destaques

--- src/components/Destaques/Destaques.jsx
+++ src/components/Destaques/Destaques.jsx
@@ -1,22 +1,36 @@
-import { Card } from './Card';
+import { Card } from './Card.jsx';
 import './Destaques.css'
 
-export const Destaques = ({ children }) => {
+export const Destaques = ({children}) => {
   return (
     <Card>
-      <div className='camisa'>
-        <p>Novo drop Supreme</p>
-        <button>Enviar</button>
-      </div>
+      <div class='cards-wraper'>
+        <div className='card-box'>
+          <p>Novo drop Supreme</p>
+          <span class='discount'>30% OFF</span>
+          <button>Comprar</button>
+          <img
+          src='../../../public/star-wars-shirt.svg'> 
+          </img>
+        </div>
 
-      <div className='tênis'>
-        <p>Coleção Adidas</p>
-        <button>Enviar</button>
-      </div>
+        <div className='card-box'>
+          <p>Coleção Adidas</p>
+          <span class='discount'>30% OFF</span>
+          <button>Comprar</button>
+          <img 
+          src='../../../public/tenis.svg'> 
+          </img>
+        </div>
 
-      <div className='fone'>
-        <p>Novo Beats Bass</p>
-        <button>Enviar</button>
+        <div className='card-box'>
+          <p>Novo Beats Bass</p>
+          <span class='discount'>30% OFF</span>
+          <button>Comprar</button>
+          <img 
+          src='../../../public/fone.svg'> 
+          </img>
+        </div>
       </div>
     </Card>
   )
