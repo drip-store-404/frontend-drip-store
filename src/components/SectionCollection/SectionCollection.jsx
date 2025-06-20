0f03b0543b09d2a3882859e8b6e02e4e1ccff0c4
SousaPatricia25 <patricia.sousa2413@outlook.com>
2025-06-20 11:33:23 -0300

    Novas atualizações dos ícones de destaques

--- src/components/SectionCollection/SectionCollection.jsx
+++ src/components/SectionCollection/SectionCollection.jsx
@@ -1,7 +1,4 @@
 
-
-import { CardCollection } from '../CardCollection/CardCollection';
-import { FeatureCollection } from '../FeatureCollection/FeatureCollection';
 import './SectionCollection.css';
 
 export const SectionCollection = () => {
@@ -10,7 +7,6 @@ export const SectionCollection = () => {
       <div className="cards-collection">
         <h2>Coleções em Destaque</h2>
         <div className="cards-container">
-
           <div className="card-item">
             <div className="card-content">
               <span className="discount">30% OFF</span>
@@ -44,17 +40,32 @@ export const SectionCollection = () => {
       </div>
 
       <div className="collections">
-        <h2>Coleções em Destaque</h2>
+        <h2>Categorias em Destaque</h2>
         <div className="features-container">
-          <FeatureCollection />
-            <img src='/tshirt-_1_.svg'/>
-          <FeatureCollection />
-            <img src='/pants.svg'/>
-          <FeatureCollection />
-            <img src='/headphones_1.svg'/>
-          <FeatureCollection />
-            <img src='/sneakers.svg'/>
-          <FeatureCollection />
+          <div className="feature-item">
+            <img src="/tshirt-_1_.svg" alt="Camisetas" className="feature-icon" />
+            <p className="feature-label">Camisetas</p>
+          </div>
+
+          <div className="feature-item">
+            <img src="/pants.svg" alt="Calças" className="feature-icon" />
+            <p className="feature-label">Calças</p>
+          </div>
+
+          <div className="feature-item">
+            <img src="/pants.svg" alt="Bonés" className="feature-icon" />
+            <p className="feature-label">Bonés</p>
+          </div>
+
+          <div className="feature-item">
+            <img src="/headphones_1.svg" alt="Headphones" className="feature-icon" />
+            <p className="feature-label">Headphones</p>
+          </div>
+
+          <div className="feature-item">
+            <img src="/sneakers.svg" alt="Tênis" className="feature-icon" />
+            <p className="feature-label">Tênis</p>
+          </div>
         </div>
       </div>
     </section>
